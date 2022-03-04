6e308a327d0e0ae0cbfb662b0bd66a919ace4266
Alexander <kotzspam@gmail.com>
2022-03-05 00:54:18 +0100

    Added models in index

--- models/index.js
+++ models/index.js
@@ -1,21 +1,23 @@
 // Setting up the database connection
-const knex = require('knex')({
+const knex = require("knex")({
 	debug: true,
-	client: 'mysql',
+	client: "mysql",
 	connection: {
-		host: process.env.DB_HOST || 'localhost',
+		host: process.env.DB_HOST || "localhost",
 		port: process.env.DB_PORT || 3306,
-		charset: process.env.DB_CHARSET || 'utf8mb4',
-		database: process.env.DB_NAME || 'boilerplate',
-		user: process.env.DB_USER || 'boilerplate',
-		password: process.env.DB_PASSWORD || '',
-	}
+		charset: process.env.DB_CHARSET || "utf8mb4",
+		database: process.env.DB_NAME || "boilerplate",
+		user: process.env.DB_USER || "boilerplate",
+		password: process.env.DB_PASSWORD || "",
+	},
 });
 
-const bookshelf = require('bookshelf')(knex);
+const bookshelf = require("bookshelf")(knex);
 
 const models = {};
-models.Example = require('./Example')(bookshelf);
+models.Album = require("./Album")(bookshelf);
+models.Photo = require("./Photo")(bookshelf);
+models.User = require("./User")(bookshelf);
 
 module.exports = {
 	bookshelf,
